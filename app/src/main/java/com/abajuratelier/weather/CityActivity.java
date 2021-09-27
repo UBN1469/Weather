2353caa2047f475c493f11bf3667e062bc702662
UBN100 <5423410@mail.ru>
2021-09-27 23:36:44 +0300

    Список городов

--- app/src/main/java/com/abajuratelier/weather/CityActivity.java
+++ app/src/main/java/com/abajuratelier/weather/CityActivity.java
@@ -1,6 +1,14 @@
 package com.abajuratelier.weather;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city);
+    }
 }
