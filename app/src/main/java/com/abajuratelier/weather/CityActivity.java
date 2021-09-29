fc6d90d9edca5db0653f3ef86b39ac9066eb67fb
UBN100 <5423410@mail.ru>
2021-09-29 16:03:13 +0300

    Экран настройки

--- app/src/main/java/com/abajuratelier/weather/CityActivity.java
+++ app/src/main/java/com/abajuratelier/weather/CityActivity.java
@@ -9,6 +9,6 @@ public class CityActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_city);
+        setContentView(R.layout.settings);
     }
 }
