abc6991965f380604b4ff6c5de91ca0c1ed82cf1
UBN100 <5423410@mail.ru>
2021-10-18 09:48:15 +0300

    SettingActivity

--- app/src/main/java/com/abajuratelier/weather/MainActivity.java
+++ app/src/main/java/com/abajuratelier/weather/MainActivity.java
@@ -2,7 +2,11 @@ package com.abajuratelier.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView setting = findViewById(R.id.setting );
+        setting.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast. makeText (MainActivity. this , "button2 ClickListener event!" ,
+//                        Toast. LENGTH_SHORT ).show();
+                startActivity(new Intent(MainActivity.this, SettingActivity.class));
+
+            }
+        });
     }
 }
--- app/src/main/java/com/abajuratelier/weather/SettingActivity.java
+++ app/src/main/java/com/abajuratelier/weather/SettingActivity.java
@@ -1,14 +1,14 @@
 package com.abajuratelier.weather;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CityActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_city);
+        setContentView(R.layout.activity_setting);
     }
 }
