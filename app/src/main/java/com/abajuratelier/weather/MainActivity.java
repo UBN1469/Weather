05ec27d7abce37a2b612c051bd94eebbe211d678
UBN100 <5423410@mail.ru>
2021-11-07 00:49:23 +0300

    Логирование жизненого цикла

--- app/src/main/java/com/abajuratelier/weather/MainActivity.java
+++ app/src/main/java/com/abajuratelier/weather/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String instanceState;
+
+        if (savedInstanceState == null) {
+            instanceState = "Первый запуск";
+        }
+        else {
+            instanceState = " Повторный запуск";
+
+
+        }
+        Toast.makeText(MainActivity.this," -onCreate() "+ instanceState, Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", " -onCreate() "+ instanceState);
+
 
         ImageView setting = findViewById(R.id.setting );
+        TextView city = findViewById(R.id.city);
         setting.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,5 +41,71 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        city.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast. makeText (MainActivity. this , "button2 ClickListener event!" ,
+//                        Toast. LENGTH_SHORT ).show();
+                startActivity(new Intent(MainActivity.this,CityActivity.class));
+
+            }
+        });
+
+    }
+    //Методы жизненого цикла:
+    @Override
+    protected void onStart() {
+        super .onStart();
+        Toast. makeText (getApplicationContext(), "onStart()" , Toast. LENGTH_SHORT ).show();
+        Log.d("MainActivity", "onStart()" );
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState){
+        super .onRestoreInstanceState(saveInstanceState);
+        Toast. makeText (getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()" , Toast. LENGTH_SHORT ).show();
+        Log.d("MainActivity", "Повторный запуск!! - onRestoreInstanceState()" );
+    }
+    @Override
+    protected void onResume() {
+        super .onResume();
+        Toast. makeText (getApplicationContext(), "onResume()" ,
+                Toast. LENGTH_SHORT ).show();
+        Log.d("MainActivity", "onResume()" );
+
+    }
+    @Override
+    protected void onPause() {
+        super .onPause();
+        Toast. makeText (getApplicationContext(), "onPause()" , Toast. LENGTH_SHORT ).show();
+        Log.d("MainActivity", "onPause()" );
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState){
+        super .onSaveInstanceState(saveInstanceState);
+        Toast. makeText (getApplicationContext(), "onSaveInstanceState()" ,
+                Toast. LENGTH_SHORT ).show();
+        Log.d("MainActivity", "onSaveInstanceState()" );
+    }
+    @Override
+    protected void onStop() {
+        super .onStop();
+        Toast. makeText (getApplicationContext(), "onStop()" , Toast. LENGTH_SHORT ).show();
+        Log.d("MainActivity", "onStop()" );
+    }
+    @Override
+    protected void onRestart() {
+        super .onRestart();
+        Toast. makeText (getApplicationContext(), "onRestart()" ,
+                Toast. LENGTH_SHORT ).show();
+        Log.d("MainActivity", "onRestart()" );
+    }
+    @Override
+    protected void onDestroy() {
+        super .onDestroy();
+        Toast. makeText (getApplicationContext(), "onDestroy()" ,
+                Toast. LENGTH_SHORT ).show();
+        Log.d("MainActivity", "onDestroy()"  );
+
     }
 }
